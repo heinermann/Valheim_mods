770017b4f1dec3669fe95019ec7053a3bd97f701
Adam Heinermann <aheinerm@gmail.com>
2022-01-29 03:05:31 -0800

    Use actual names in the piece description where applicable

--- BetterCreative/Prefabs.cs
+++ BetterCreative/Prefabs.cs
@@ -241,6 +241,43 @@ namespace Heinermann.BetterCreative
       PrefabManager.Instance.AddPrefab(ghost);
     }
 
+    private static string GetPrefabFriendlyName(GameObject prefab)
+    {
+      HoverText hover = prefab.GetComponent<HoverText>();
+      if (hover) return hover.m_text;
+
+      ItemDrop item = prefab.GetComponent<ItemDrop>();
+      if (item) return item.m_itemData.m_shared.m_name;
+
+      Character chara = prefab.GetComponent<Character>();
+      if (chara) return chara.m_name;
+
+      RuneStone runestone = prefab.GetComponent<RuneStone>();
+      if (runestone) return runestone.m_name;
+
+      ItemStand itemStand = prefab.GetComponent<ItemStand>();
+      if (itemStand) return itemStand.m_name;
+
+      MineRock mineRock = prefab.GetComponent<MineRock>();
+      if (mineRock) return mineRock.m_name;
+
+      Pickable pickable = prefab.GetComponent<Pickable>();
+      if (pickable) return GetPrefabFriendlyName(pickable.m_itemPrefab);
+
+      CreatureSpawner creatureSpawner = prefab.GetComponent<CreatureSpawner>();
+      if (creatureSpawner) return GetPrefabFriendlyName(creatureSpawner.m_creaturePrefab);
+
+      SpawnArea spawnArea = prefab.GetComponent<SpawnArea>();
+      if (spawnArea && spawnArea.m_prefabs.Count > 0) {
+        return GetPrefabFriendlyName(spawnArea.m_prefabs[0].m_prefab);
+      }
+
+      Piece piece = prefab.GetComponent<Piece>();
+      if (piece && !string.IsNullOrEmpty(piece.m_name)) return piece.m_name;
+
+      return prefab.name;
+    }
+
     private static void CreatePrefabPiece(GameObject prefab)
     {
       InitPieceData(prefab);
@@ -248,7 +285,7 @@ namespace Heinermann.BetterCreative
       var pieceConfig = new PieceConfig
       {
         Name = prefab.name,
-        Description = prefab.name,
+        Description = GetPrefabFriendlyName(prefab),
         PieceTable = "_HammerPieceTable",
         Category = GetPrefabCategory(prefab),
         AllowedInDungeons = true,
