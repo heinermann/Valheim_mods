49602823d88e6f28f37d93d8ef394b7574612ccc
Adam Heinermann <aheinerm@gmail.com>
2022-02-09 19:41:13 -0800

    [Ruins] Blacklist wood torches, new jank terrain algorithm

--- TheRuins/Ruins.cs
+++ TheRuins/Ruins.cs
@@ -188,7 +188,7 @@ namespace Heinermann.TheRuins
     };
 
     static HashSet<string> blacklistedPieces = new HashSet<string>() {
-      "piece_gift1", "piece_gift2", "piece_gift3", "piece_xmastree", "piece_jackoturnip"
+      "piece_gift1", "piece_gift2", "piece_gift3", "piece_xmastree", "piece_jackoturnip", "piece_groundtorch_wood"
     };
 
     private bool IsBlackListedPiece(PieceEntry piece)
@@ -526,80 +526,125 @@ namespace Heinermann.TheRuins
       terrain.m_square = false;
       terrain.m_paintCleared = false;
       terrain.m_playerModifiction = false;
+      terrain.m_spawnAtMaxLevelDepth = true;
 
       var znet = prefab.AddComponent<ZNetView>();
       znet.m_persistent = true;
       znet.m_type = ZDO.ObjectType.Default;
 
-      return GameObject.Instantiate(prefab, parent.transform, false);
+      GameObject result = GameObject.Instantiate(prefab, parent.transform, false);
+      result.transform.position = Vector3.zero;
+      result.transform.rotation = Quaternion.identity;
+      return result;
     }
 
-    private void FlattenArea(GameObject prefab, float relativeTargetLevel)
+    private TerrainModifier FlattenAreaAt(GameObject prefab, float relativeTargetLevel, Vector3 position, float radius, string name)
     {
       GameObject pieceObj = CreateTerrainModifierPrefab(prefab);
-      pieceObj.transform.position = Vector3.zero;
-      pieceObj.transform.rotation = Quaternion.identity;
-      pieceObj.name = "LevelTerrain";
+      pieceObj.transform.position = position;
+      pieceObj.name = name;
 
       var modifier = pieceObj.GetComponent<TerrainModifier>();
       modifier.m_level = true;
-      modifier.m_levelRadius = GetFlattenRadius() + 2f;
+      modifier.m_levelRadius = radius;
       modifier.m_levelOffset = relativeTargetLevel;
+
+      modifier.m_sortOrder = Mathf.RoundToInt(-(position.y + relativeTargetLevel) * 10f);
 
       modifier.m_smooth = true;
       modifier.m_smoothPower = 3f;
-      modifier.m_smoothRadius = GetFlattenRadius() + 10f;
+      modifier.m_smoothRadius = radius + 2f;
+      return modifier;
     }
 
-    // Naiive solution, doesn't consider geometries (i.e. 4-long stone wall) and has fairly major roudning to integer
+    private TerrainModifier FlattenArea(GameObject prefab, float relativeTargetLevel)
+    {
+      TerrainModifier modifier = FlattenAreaAt(prefab, relativeTargetLevel, Vector3.zero, GetFlattenRadius() + 2f, "TheRuins_Flatten_Area");
+      modifier.m_smooth = true;
+      modifier.m_smoothPower = 3f;
+      modifier.m_smoothRadius = modifier.m_levelRadius + 4f;
+      return modifier;
+    }
+
+    private struct LevelData
+    {
+      public Vector3 position;
+      public WearNTear component;
+
+      public LevelData(float x, float y, float z, WearNTear component)
+      {
+        position = new Vector3(x, y, z);
+        this.component = component;
+      }
+    }
+
+    // Granularity of below algorithms, higher = more fidelity but more terrain modifier instances
+    const float terrainGranularity = 2f;
+
+    private void CompareComponentHeights(Dictionary<Tuple<int, int>, LevelData> lowestPositions, WearNTear wear, int x, int z)
+    {
+      var point = Tuple.Create(x, z);
+
+      // TODO optimize this so it isn't recalc'd
+      float thisY = wear.transform.position.y - GetPieceHeight(wear.gameObject) / 2f;
+
+      LevelData compareWith;
+      if (lowestPositions.TryGetValue(point, out compareWith))
+      {
+        if (thisY < compareWith.position.y)
+          lowestPositions[point] = new LevelData(x / terrainGranularity, thisY, z / terrainGranularity, wear);
+      }
+      else
+      {
+        lowestPositions.Add(point, new LevelData(x / terrainGranularity, thisY, z / terrainGranularity, wear));
+      }
+    }
+
+    // Naiive solution, doesn't consider geometries (i.e. 4-long stone wall) and has fairly major rounding errors to integer
     private void PrepareTerrainModifiers(GameObject prefab)
     {
-      var lowestComponents = new Dictionary<Tuple<int, int>, WearNTear>();
-      foreach (WearNTear wear in prefab.GetComponentsInChildren<WearNTear>())
+      // Get the leveling data from each position
+      var lowestPositions = new Dictionary<Tuple<int, int>, LevelData>();
+      foreach (WearNTear wear in prefab.GetComponentsInChildren<WearNTear>(includeInactive: true))
       {
-        WearNTear compareWith;
-        var point = Tuple.Create(Mathf.RoundToInt(wear.transform.position.x), Mathf.RoundToInt(wear.transform.position.z));
-        if (lowestComponents.TryGetValue(point, out compareWith))
+        int x = Mathf.RoundToInt(wear.transform.position.x * terrainGranularity);
+        int z = Mathf.RoundToInt(wear.transform.position.z * terrainGranularity);
+        for (int i = -1; i <= 1; ++i)
         {
-          if (wear.transform.position.y < compareWith.transform.position.y)
-            lowestComponents[point] = wear;
-        }
-        else
-        {
-          lowestComponents.Add(point, wear);
+          for (int j = -1; j <= 1; ++j)
+          {
+            CompareComponentHeights(lowestPositions, wear, x + i, z + j);
+          }
         }
       }
 
-      float targetY = lowestComponents.Values
-        .Select(w => w.transform.position.y)
-        .Where(y => y < 1f)
-        .Average();
+      // Flatten entire area at high priority first
+      float targetY = Mathf.Max(lowestPositions.Values
+        .Select(w => w.position.y)
+        .Where(y => y < 0.5f)
+        .Average(), 0);
 
-      FlattenArea(prefab, targetY);
+      TerrainModifier areaFlatten = FlattenArea(prefab, targetY);
+      areaFlatten.m_sortOrder = -1000;
 
-      /*
-      foreach (WearNTear wear in lowestComponents.Values)
+      // Flatten areas with pieces in it
+      int index = 0;
+      foreach (LevelData lowest in lowestPositions.Values)
       {
-        if (wear.transform.position.y > 2f) continue;
+        if (lowest.position.y > 1f) continue;
 
-        CustomTerrainOp terrain = wear.gameObject.AddComponent<CustomTerrainOp>();
-
-        terrain.m_settings.m_level = true;
-        terrain.m_settings.m_levelRadius = Mathf.Max(GetPieceRadius(wear.gameObject) + 0.2f, 1.2f);
-        terrain.m_settings.m_levelOffset = -GetPieceHeight(wear.gameObject);
-
-        terrain.m_settings.m_smooth = true;
-        terrain.m_settings.m_smoothRadius = terrain.m_settings.m_levelRadius * 2f;
-        terrain.m_settings.m_smoothPower = 3f;
-
-        terrain.m_settings.m_paintCleared = true;
-        terrain.m_settings.m_paintRadius = terrain.m_settings.m_levelRadius;
-        terrain.m_settings.m_paintType = TerrainModifier.PaintType.Reset;
-        if (wear.m_materialType == WearNTear.MaterialType.Stone || wear.GetComponent("Fireplace"))
+        float radius = 2f / terrainGranularity;
+        TerrainModifier mod = FlattenAreaAt(prefab, 0, lowest.position, radius, $"TheRuins_Flatten_{index}");
+        if (lowest.component.m_materialType == WearNTear.MaterialType.Stone || lowest.component.GetComponent("Fireplace"))
         {
-          terrain.m_settings.m_paintType = TerrainModifier.PaintType.Dirt;
+          mod.m_paintCleared = true;
+          mod.m_paintRadius = GetPieceRadius(lowest.component.gameObject) + 0.5f;
+          mod.m_paintType = TerrainModifier.PaintType.Dirt;
         }
-      }*/
+        ++index;
+      }
+
+      // TODO smooth surrounding border
     }
 
     public void FullyRuinBlueprintToLocation()
