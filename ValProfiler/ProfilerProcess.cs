f1bad6356fabbe64fc0b3678dfefa188e6704b9e
Adam Heinermann <aheinerm@gmail.com>
2022-04-04 00:39:26 -0700

    Experiment with valprofiler rewrite

---
+++ ValProfiler/ProfilerProcess.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.Diagnostics.Runtime;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+
+namespace ValProfiler
+{
+  using ProfileDict = Dictionary<string, ulong>;
+
+  static class ProfilerProcess
+  {
+    static readonly string dumpDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/dumps";
+    static readonly string dumpTimePrefix = DateTime.Now.ToString("yyyy-MM-dd_HH-mm");
+
+    public static string getDumpPath(int threadId)
+    {
+      string threadName = $"thread{threadId}";
+      return $"{dumpDirectory}/{dumpTimePrefix}_{threadName}_profile.folded";
+    }
+
+    static void Main(string[] args)
+    {
+      if (args == null)
+      {
+        Console.WriteLine("Args are NULL");
+        return;
+      }
+      Console.WriteLine("ARGS: " + string.Join(", ", args));
+
+      if (args.Length < 3)
+      {
+        Console.WriteLine("Invalid args");
+        return;
+      }
+
+      int pid = int.Parse(args[0]);
+      int msPerTrace = int.Parse(args[1]);
+      int msPerDump = int.Parse(args[2]) * 1000;
+      
+      Directory.CreateDirectory(dumpDirectory);
+
+      using (var dataTarget = DataTarget.AttachToProcess(pid, false))
+      {
+        ClrRuntime runtime = dataTarget.ClrVersions[0].CreateRuntime();
+        ProfileRuntime(runtime, msPerTrace, msPerDump);
+      }
+    }
+
+    static void ProfileRuntime(ClrRuntime runtime, int msPerTrace, int msPerDump)
+    {
+      var profileData = new Dictionary<int, ProfileDict>();
+      Stopwatch dumpTimer = Stopwatch.StartNew();
+      Stopwatch elapsed = Stopwatch.StartNew();
+
+      while (runtime.Threads.Length > 0)
+      {
+        Thread.Sleep((int)Math.Max(0, msPerTrace - elapsed.ElapsedMilliseconds));
+        elapsed.Restart();
+
+        foreach (var thread in runtime.Threads)
+        {
+          ProfileThread(thread, profileData);
+        }
+
+        if (dumpTimer.ElapsedMilliseconds > msPerDump)
+        {
+          dumpTimer.Restart();
+          WriteDump(profileData);
+        }
+      }
+    }
+
+    static void ProfileThread(ClrThread thread, Dictionary<int, ProfileDict> profileData)
+    {
+      ProfileDict flatTraces;
+      if (!profileData.TryGetValue(thread.ManagedThreadId, out flatTraces))
+      {
+        flatTraces = new ProfileDict();
+        profileData.Add(thread.ManagedThreadId, flatTraces);
+      }
+
+      string trace = GetFlatStackTrace(thread.EnumerateStackTrace());
+
+      flatTraces.TryGetValue(trace, out ulong count);
+      flatTraces[trace] = count + 1;
+    }
+
+    static string CreateFlatFile(ProfileDict profile)
+    {
+      StringBuilder result = new StringBuilder();
+      foreach(var pair in profile)
+      {
+        result.Append(pair.Key);
+        result.Append(' ');
+        result.Append(pair.Value);
+        result.Append('\n');
+      }
+      return result.ToString();
+    }
+
+    static void WriteDump(Dictionary<int, ProfileDict> profileData)
+    {
+      foreach(var profilePair in profileData)
+      {
+        int threadId = profilePair.Key;
+        var traces = profilePair.Value;
+
+        string dumpFilePath = getDumpPath(threadId);
+        try
+        {
+          File.WriteAllText(dumpFilePath, CreateFlatFile(traces));
+        } catch {}
+      }
+    }
+
+    static string GetFlatStackTrace(IEnumerable<ClrStackFrame> stack)
+    {
+      ArrayList result = new ArrayList();
+      foreach (var frame in stack)
+      {
+        result.Add(frame.Method?.Signature ?? $"{frame.InstructionPointer}");
+      }
+      return string.Join(";", result.Cast<string>().Reverse());
+    }
+  }
+}
--- ValProfiler/ValProfiler.cs
+++ ValProfiler/ValProfiler.cs
@@ -1,20 +1,31 @@
 ﻿using BepInEx;
-using HarmonyLib;
+using BepInEx.Configuration;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace Heinermann.ValProfiler
 {
-  [BepInPlugin("heinermann.valprofiler", "Valheim Profiler", "0.1")]
-  [BepInProcess("valheim.exe")]
+  [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
   public class ValProfiler : BaseUnityPlugin
   {
-    public static Harmony harmony = new Harmony("mod.heinermann.valprofiler");
+    public const string PluginGUID = "com.heinermann.valprofiler";
+    public const string PluginName = "ValProfiler";
+    public const string PluginVersion = "2.0.0";
 
-    static ProfilerBase profiler = new DetourProfiler();
+    const string Options = "Options";
+    public static ConfigEntry<int> MillisecondsPerTrace;
+    public static ConfigEntry<int> SecondsPerDump;
 
     void Awake()
     {
-      harmony.PatchAll();
-      profiler.DoPrePatch(harmony);
+      MillisecondsPerTrace = Config.Bind(Options, "MillisecondsPerTrace", 20);
+      SecondsPerDump = Config.Bind(Options, "SecondsPerDump", 30);
+
+      Process proc = new Process();
+      proc.StartInfo.FileName = Assembly.GetExecutingAssembly().Location;
+      proc.StartInfo.Arguments = $"{Process.GetCurrentProcess().Id} {MillisecondsPerTrace.Value} {SecondsPerDump.Value}";
+      proc.StartInfo.UseShellExecute = false;
+      proc.Start();
     }
   }
 }
